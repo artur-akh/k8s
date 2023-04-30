978bc71d5e1aad66929d6a8ea9d5a2d24b6d3da8
Artur Akhmadiev <artur_akh@live.ru>
2023-05-01 00:48:21 +0300

    add to stress-ng and new new v5 vesrion

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,10 @@ FROM nginx
 
 COPY entrypoint.sh .
 
-RUN chmod +x entrypoint.sh
+RUN apt-get update && \
+    apt-get install -y stress-ng && \
+    rm -rf /var/lib/apt/lists/* && \
+    chmod +x ./entrypoint.sh 
 
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ IP_ADDRESS=$(hostname -i)
 echo "<html>
   <body bgcolor="white">
     <center>
-      <h1><font color="black">v4</font></h1>
+      <h1><font color="black">v5</font></h1>
       <p>IP ADDRESS: $IP_ADDRESS</p>
       <p>HOSTNAME: $HOSTNAME</p>
     </center>
